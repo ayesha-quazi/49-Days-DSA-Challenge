73e7016e041e62425fdc1ea0d0ac620f16ddad91
Ayesha Quazi <aayeshaquazi04@gmail.com>
2024-12-31 11:12:08 +0530

    code added

--- Day-45.cpp
+++ Day-45.cpp
@@ -1,4 +1,34 @@
 // i. Lowest Common Ancestor of a Binary Search Tree
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)
+            return NULL;
+        
+        if(root->val == p->val || root->val == q->val)
+            return root;
+        
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
+        
+        if(l && r)
+            return root;
+        
+        return l?l:r;
+    }
+};
+
 
 // ii. Recover Binary Search Tree
